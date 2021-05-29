b64918f29220c827929086e72ffe04e79c61fbf8
Bhushan C. Mhatre <44663470+thebhushanmhatre@users.noreply.github.com>
2021-05-30 00:04:45 +0530

    Adding Styling of bars and sort logic steps

--- components/Bars.js
+++ components/Bars.js
@@ -1,12 +1,31 @@
+import React, { useState, useEffect } from 'react'
 
 function Bar(props) {
+  
+  const [width, setWidth] = React.useState(0)
+
+  React.useEffect(() => {
+    setWidth(window.innerWidth)
+  })
+
+  let extra = props.obj.compareItems.includes(props.pos) ? " bar-compare" : ""
+  extra += props.obj.sortedIndex >= props.pos ? " bar-sorted" : ""
+  let bar_width = { 
+    "width": String(75 + props.num * width * 0.0035) + "px"
+  }
   return (
-    <>
-      <p className='bar' >{props.num}</p>
-    </>
+    <p>
+      <span className={"bar" + extra} style={bar_width}>
+        {props.num}
+      </span>
+    </p>
   )
 }
 
 export default function Bars(props) {
-  return <div>{props.items.map((i,j) => <Bar num={i} key={j} />)}</div>
+  return (
+    <div>
+      {props.items.map((i,j) => <Bar num={i} pos={j} key={j} obj={props.obj} />)}
+    </div>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,120 @@
 import Head from 'next/head'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import Bars from '../components/Bars'
 
 function generateArray(num) {
-  console.log('Inside Generate Array')
-  let arr = []
-  for (let i = 0; i < num; i++) {
-    arr.push(Math.round(Math.random() * 200 + 5))
+  if (validate_n_set(num)){
+    console.log('Inside Generate Array')
+    let arr = []
+    for (let i = 0; i < num; i++) {
+      arr.push(Math.round(Math.random() * 200 + 5))
+    }
+    return arr
   }
-  return arr
 }
 
+function validate_n_set(num, setNum) {
+  if (num > 4 && num < 26) {
+    return true
+  }
+  return false
+}
+
+function getsortArray(items){
+  let sorted_items = [...items]
+  sorted_items.sort((a, b) => { return a - b })
+  return sorted_items
+}
+
+function isSorted(arr){
+  let sorted_items = [...arr].sort((a, b) => { return a - b })
+  return (arr === sorted_items ? true : false)
+}
 
 export default function Home() {
 
-  var [num, setNum] = useState(10)
-  var [items, setItems] = useState([159, 190, 20, 198, 14, 25, 70, 134, 164, 190])
+  var [num, setNum] = useState(20)
+  var [items, setItems] = useState([...Array(20)].map(() => Math.floor(Math.random() * 200 + 5)))
+  var [compareItems, setCompareItems] = useState([-1, -1])
+  var [sortedIndex, setSortedIndex] = useState([-1])
 
-  let sortArray = (e) => {
+  // FAILURE:
+  // Cannot Change State form Loops or Conditions
+
+  let resetArray = () => {
+    setItems(generateArray(num))
+    setCompareItems([-1, -1])
+    setSortedIndex(-1)
+  }
+  
+  let finishSorting = () => {
+    let sorted_items = getsortArray(items)
+    setItems(sorted_items)
+    setCompareItems([0, 0])
+    setSortedIndex(sorted_items.length)
+  }
+
+  let startSorting = () => {
+    while (!isSorted(items)) {
+      let [i, j] = [...compareItems]
+      let newItems = [...items]
+      
+      if (sortedIndex != items.length){
+        if (i < 0){
+          i = 0
+          setCompareItems([...[i, j]])
+        }
+        if (j < 0 || j < i){
+          j = i + 1
+          setCompareItems([...[i, j]])
+        }
+        if (i >= 0 && i < items.length-2){
+          if (j < items.length-1){
+            j+=1
+            
+            setCompareItems([...[i, j]])
+            
+            // setTimeout(function () {
+              if (newItems[i] > newItems[j]) {
+                let temp = newItems[j]
+                newItems[j] = newItems[i]
+                newItems[i] = temp
+
+                setCompareItems([...[i, j]])
+                setItems(newItems)
+              }
+            // }, 500)
+            
+          } else if (j === items.length){
+            j = i+2
+            i+=1
+
+            // setTimeout(function () {
+              if (newItems[i] > newItems[j]) {
+                let temp = newItems[j]
+                newItems[j] = newItems[i]
+                newItems[i] = temp
+
+                setItems(newItems)
+                setCompareItems([...[i, j]])
+                setSortedIndex(i)
+              }
+            // }, 500)
+          }
+        }
+      }
+
+      console.log(`i=${i}, j=${j} setCompareItems=${[...[i,j]]} compareItems=${compareItems}  sortedIndex=${sortedIndex}`)
+
+      setItems(newItems)
+      setCompareItems([...[i, j]])
+
+      console.log(`i=${i}, j=${j} setCompareItems=${[...[i,j]]} compareItems=${compareItems}  sortedIndex=${sortedIndex}`)
+      console.log('done')
+
+      // setTimeout(() => { startSorting() }, 3000);
+
+    }
   }
 
   return (
@@ -32,14 +129,21 @@ export default function Home() {
         <h2>Sorting Visualizer</h2>
         <div className="navbar-items">
           <label htmlFor="length">Number of elements </label>
-          <input type="number" id="length" name="length" min="5" max="200" placeholder="20" onChange={(e) => setNum(e.target.value)} required />
-          <button type="submit" onClick={() =>setItems(generateArray(num))}> Generate Array</button>
-          <button onClick={(e) => sortArray(e)}>Start Sorting</button>
+
+          <div className="tooltip">
+            <input type="number" id="length" name="length" min="5" max="25" placeholder="20"
+              onChange={(e) => validate_n_set(e.target.value) ? setNum(e.target.value) : undefined} required />
+            <span className="tooltiptext">min 5 - max 25</span>
+          </div>
+
+          <button type="submit" onClick={() => resetArray()}> Regenerate ({num})</button>
+          <button type="submit" onClick={() => startSorting()} disabled>Start Sorting </button>
+          <button type="submit" onClick={() => finishSorting()}>Finish </button>
         </div>
       </div>
 
       <div className='chart'>
-        <Bars items={items}/>
+        <Bars items={items} obj={{ 'compareItems': compareItems, 'sortedIndex': sortedIndex}} />
       </div>
 
     </div>
